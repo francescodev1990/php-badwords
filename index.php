c274c5603f520006b1c7b611e4743e1da5f7e993
Your Name <almannofrancesco@gmail.com>
2023-01-27 15:51:24 +0100

    index php and wordsegret created

---
+++ index.php
@@ -0,0 +1,33 @@
+<!-- SEZIONE PHP -->
+
+<?php
+
+
+?>
+
+<!-- SEZIONE HTML -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+
+<body>
+    <form action="./wordsegret.php" method="POST">
+        <input type="text" name="" placeholder="Definizione">
+        <input type="text" name="" placeholder="Password">
+        <button type="submit">Conferma Invio!</button>
+
+    </form>
+    <form action="" method="POST">
+
+    </form>
+
+</body>
+
+</html>
